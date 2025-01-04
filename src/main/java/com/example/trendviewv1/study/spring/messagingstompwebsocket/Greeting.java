daf2d57e47ec6b07288025f49864c007b6f43efe
동준 <color6763@naver.com>
2025-01-04 11:26:52 +0900

    init : Greeting class 추가

---
+++ src/main/java/com/example/trendviewv1/study/spring/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,16 @@
+package com.example.trendviewv1.study.spring.messagingstompwebsocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {}
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
